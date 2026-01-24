678e48fe5371ffcecebed3ceafbd58db0e36fef8
anshu706 <anshupadhayay212@gmail.com>
2026-01-24 22:29:39 +0530

    142

---
+++ 142_linked_list_cycle_2.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    ListNode *detectCycle(ListNode *head)
+    {
+        ListNode *f = head;
+        ListNode *s = head;
+
+        while (f != NULL && f->next != NULL)
+        {
+            s = s->next;
+            f = f->next->next;
+
+            if (s == f)
+            {
+                ListNode *start = head;
+                while (start != s)
+                {
+                    start = start->next;
+                    s = s->next;
+                }
+                return start;
+            }
+        }
+        return NULL;
+    }
+};
