9d6d624af0d0730aee338527790844d94bab2c94
anshu@706 <anshupadhayay212@gmail.com>
2026-01-26 17:38:14 +0530

    236

---
+++ 236_lowest_common_ancestor.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
+        if (root == NULL)
+            return NULL;
+        if (root == p)
+            return p;
+        if (root == q)
+            return q;
+
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
+
+        if (left == NULL)
+            return right;
+        if (right == NULL)
+            return left;
+
+        return root;
+    }
+};
