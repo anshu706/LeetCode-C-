b5ce8ec379cff76d720cbf24f816c0830c32f609
ansh <anshupadhayay>
2026-02-13 20:21:28 +0530

    145

---
+++ 145_binary_tree_postorder_traversal.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    void dfs(TreeNode *root, vector<int> &res)
+    {
+        if (root == NULL)
+            return;
+
+        dfs(root->left, res);
+        dfs(root->right, res);
+        res.push_back(root->val);
+    }
+
+    vector<int> postorderTraversal(TreeNode *root)
+    {
+        vector<int> res;
+        dfs(root, res);
+        return res;
+    }
+};
